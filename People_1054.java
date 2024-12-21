12d2fb6affa4840eed2c41e1c3cb5682656e8a8b
Volkov Anton <volkovmmtr@gmail.com>
2024-12-21 11:22:42 +0300

    add print3

--- People_1054.java
+++ People_1054.java
@@ -1,6 +1,6 @@
 public class People_1054 {
     public static void main(String[] args) {
-        System.out.println("Hello big planet");
+        System.out.println("Hello very big planet");
         System.out.println("Hello new world");
     }
 }
